cfce252d78f33876d0aed3decf35ca4ef46f777b
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-08-10 15:05:18 +0900

    Feat: Make boilerplate for the course

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Canvas } from '@react-three/fiber';
+
 function App() {
-	return <div></div>;
+	return (
+		<Canvas camera={{ position: [0, 0, 2] }}>
+			<mesh>
+				<boxGeometry />
+				<meshBasicMaterial color={0x00ff00} wireframe />
+			</mesh>
+		</Canvas>
+	);
 }
 
 export default App;
