0924be986bcb20a63fbab321a38c9c3da012ff12
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-08-10 15:58:10 +0900

    Feat: pass props to adjust mesh component

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,6 +1,10 @@
-const Box = () => {
+import { useState } from 'react';
+
+const Box = ({ position, name }) => {
+	// position[]: [x, y, z], name: 'A' or 'B' or else
+
 	return (
-		<mesh>
+		<mesh position={position} name={name}>
 			<boxGeometry />
 			<meshBasicMaterial color={0x00ff00} wireframe />
 		</mesh>
