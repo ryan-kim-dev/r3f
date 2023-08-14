2b50deb4a4852acf7510b85672fd7dcf9896851f
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-08-14 16:00:10 +0900

    Feat: useFrame basic useage

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,19 +1,24 @@
 import { useRef, useEffect } from 'react';
+import { useFrame } from '@react-three/fiber';
 
 const Box = ({ position, name }) => {
-	const instanceRef = useRef();
-	const materialRef = useRef();
+  const instanceRef = useRef();
+  const materialRef = useRef();
 
-	useEffect(() => {
-		console.log({ instanceRef, materialRef });
-	}, []);
+  useFrame((state, delta) => {
+    // useFrame 훅은 state 와 delta를 파라미터로 받는다
+    // state는 현재 프레임의 상태를 나타내는 객체이다
+    instanceRef.current.rotation.x += 1 * delta;
+    instanceRef.current.rotation.y += 0.5 * delta;
+    instanceRef.current.position.y = Math.sin(state.clock.getElapsedTime()) / 2;
+  });
 
-	return (
-		<mesh position={position} name={name} ref={instanceRef}>
-			<boxGeometry />
-			<meshBasicMaterial color={0x00ff00} wireframe ref={materialRef} />
-		</mesh>
-	);
+  return (
+    <mesh position={position} name={name} ref={instanceRef}>
+      <boxGeometry />
+      <meshBasicMaterial color={0x00ff00} wireframe ref={materialRef} />
+    </mesh>
+  );
 };
 
 export default Box;
