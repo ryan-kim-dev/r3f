d4728c9584bd1dec12544c77e305d170855d924c
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-08-14 16:36:49 +0900

    Feat: basic useage of r3f events

--- src/Box.jsx
+++ src/Box.jsx
@@ -14,7 +14,13 @@ const Box = ({ position, name }) => {
   });
 
   return (
-    <mesh position={position} name={name} ref={instanceRef}>
+    <mesh
+      position={position}
+      name={name}
+      ref={instanceRef}
+      onPointerDown={(e) => console.log(e.object.name)}
+      onUpdate={(self) => console.log(self)}
+    >
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe ref={materialRef} />
     </mesh>
@@ -22,3 +28,36 @@ const Box = ({ position, name }) => {
 };
 
 export default Box;
+
+/* 공식문서: https://docs.pmnd.rs/react-three-fiber/api/events
+ 
+  useFrame 훅은 state 와 delta를 파라미터로 받는다<mesh
+  onClick={(e) => console.log('click')}
+  onContextMenu={(e) => console.log('context menu')}
+  onDoubleClick={(e) => console.log('double click')}
+  onWheel={(e) => console.log('wheel spins')}
+  onPointerUp={(e) => console.log('up')}
+  onPointerDown={(e) => console.log('down')}
+  onPointerOver={(e) => console.log('over')}
+  onPointerOut={(e) => console.log('out')}
+  onPointerEnter={(e) => console.log('enter')} // see note 1
+  onPointerLeave={(e) => console.log('leave')} // see note 1
+  onPointerMove={(e) => console.log('move')}
+  onPointerMissed={() => console.log('missed')}
+  onUpdate={(self) => console.log('props have been updated')}
+/>
+
+({
+  ...DomEvent                   // All the original event data
+  ...Intersection                 // All of Three's intersection data - see note 2
+  intersections: Intersection[]    // The first intersection of each intersected object
+  object: Object3D              // The object that was actually hit
+  eventObject: Object3D         // The object that registered the event
+  unprojectedPoint: Vector3     // Camera-unprojected point
+  ray: Ray                      // The ray that was used to strike the object
+  camera: Camera                // The camera that was used in the raycaster
+  sourceEvent: DomEvent         // A reference to the host event
+  delta: number                 // Distance between mouse down and mouse up event in pixels
+}) => ...
+
+ */
