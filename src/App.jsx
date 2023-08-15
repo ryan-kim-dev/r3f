629e6158bfdf515b265af6bfac1163b2393f4676
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-08-15 15:28:45 +0900

    Feat: Add Stats component to check performance in dev environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Canvas } from '@react-three/fiber';
-import Polyhedron from './Polyhedron';
+import { Stats } from '@react-three/drei';
 import * as THREE from 'three';
+import Polyhedron from './Polyhedron';
 
 export default function App() {
   const polyhedron = [
@@ -15,6 +16,7 @@ export default function App() {
       <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
       <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
       <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
+      <Stats />
     </Canvas>
   );
 }
